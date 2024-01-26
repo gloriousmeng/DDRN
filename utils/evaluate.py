0898deede5a04bb74e1c597d42baae1ddacd3739
从文 <congwen.mh@alibaba-inc.com>
2024-01-26 14:20:56 +0800

    add: data loader & run script

---
+++ utils/evaluate.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# @Time: 2024/1/26 14:18
+# @Author: Hui Meng
+# @FileName: evaluate.py
+# @Software: PyCharm Community Edition
+from sklift.metrics import uplift_auc_score
+
+
+class Evaluator(object):
+    def __init__(self, t, yf, ycf=None, mu1=None, mu0=None):
+        self.y = yf
+        self.t = t
+        self.y_cf = ycf
+        self.mu0 = mu0
+        self.mu1 = mu1
+        if mu0 is not None and mu1 is not None:
+            self.true_ite = mu1 - mu0
+
+    def auuc(self, y1_pred, y0_pred):
+        uplift_score = y1_pred - y0_pred
+        auuc = uplift_auc_score(y_true=self.y, uplift=uplift_score, treatment=self.t)
+        return auuc
--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,11 @@
 # @Author: Hui Meng
 # @FileName: utils.py
 # @Software: PyCharm Community Edition
+import logging
 import os
+
+import pandas as pd
+from torch import nn
 
 
 def get_oldest_file(directory, threshold=10):
@@ -22,3 +26,42 @@ def get_oldest_file(directory, threshold=10):
     if oldest is not None and len(os.listdir(directory)) > threshold:
         os.remove(oldest[0])
         print("We have delete oldest file：", oldest[0])
+
+
+def simple_lookup(y0, y1, treatment):
+    assert y0.__len__() == y1.__len__()
+    assert y0.__len__() == treatment.__len__()
+    yf, ycf = [], []
+    for ind, v in enumerate(treatment):
+        if v == 1:
+            yf.append(y1[ind])
+            ycf.append(y0[ind])
+        else:
+            yf.append(y0[ind])
+            ycf.append(y1[ind])
+    return pd.Series(yf), pd.Series(ycf)
+
+
+def cim_logger(log_file):
+    logger = logging.getLogger('CIM')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
+    file_handler = logging.FileHandler(filename=log_file)
+    file_handler.setLevel(level=logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def weights_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_normal_(m.weight)
+        nn.init.constant_(m.bias, 0.0)
